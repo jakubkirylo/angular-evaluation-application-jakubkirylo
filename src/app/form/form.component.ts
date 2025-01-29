7794816e55492f5d445c5a9bae24fded3f84796d
Jakub Kiryło <jakub.kirylo@gmail.com>
2025-01-29 13:05:14 +0100

    Part 4.1 Complete

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AppState } from '../store/reducers/app.reducers';
+import { Store } from '@ngrx/store';
+import { Observable, shareReplay } from 'rxjs';
+import { selectIsBusy } from '../store/selectors/app.selectors';
 
 @Component({
   selector: 'app-form',
@@ -9,15 +23,18 @@ import { ReactiveFormsModule } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormComponent {
-  public red = signal('');
-  green = signal('');
-  blue = signal('');
+  private readonly fb = inject(FormBuilder);
+  private store = inject(Store<{ app: AppState }>);
 
-  public submit(): void {
-    console.warn('Submitted values:', {
-      red: this.red(),
-      green: this.green(),
-      blue: this.blue(),
-    });
-  }
+  protected isBusy$: Observable<boolean> = this.store
+    .select(selectIsBusy)
+    .pipe(shareReplay(1));
+
+  protected form: FormGroup = this.fb.group({
+    red: ['', [Validators.required, Validators.min(3), Validators.max(15)]],
+    green: ['', [Validators.required, Validators.min(3), Validators.max(15)]],
+    blue: ['', [Validators.required, Validators.min(3), Validators.max(15)]],
+  });
+
+  public submit(): void {}
 }
